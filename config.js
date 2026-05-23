ae06b1ded901860d233bca19e15173726193369d
nattapong2848 <nattapong2848@gmail.com>
2026-05-23 16:48:19 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,1 @@
-window.API_URL = "https://script.google.com/macros/s/AKfycbx8oOFb7XvANoagptA795gdYhvbnsVC4sYNoxsSEP6HpaHFPEKgh1_GMe5AYgyrBZNgsA/exec";
+window.API_URL = "https://script.google.com/macros/s/AKfycbxY6uJj7XwZe4Zjm5r7tCwYh8KQ8BXEmjlxt37GL-sRDZsHD5wX25zOYhsPwuqLZVSVxg/exec";
